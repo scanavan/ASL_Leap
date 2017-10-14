d599f73fa5d77e58e3d190c28afdda8148768ad7
Jacob <jacobschioppo@gmail.com>
2017-10-14 12:28:11 -0400

    Added Text Controls
    Added new component to scene that contains script to control the text in our room scene

---
+++ Headset/Assets/TextManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour {
+
+    public Text GestureObj;
+
+	// Use this for initialization
+	void Start () {
+        GestureObj.text = "B";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
